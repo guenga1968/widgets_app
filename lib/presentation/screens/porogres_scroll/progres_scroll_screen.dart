21644de990210e3f6e859344e1db25896422f9b5
guenga1968 <gustavogallesio@hotmail.com>
2023-06-11 17:01:07 -0300

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/porogres_scroll/progres_scroll_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProgresScrollScreen extends StatelessWidget {
+
+  static const String name ='progres_screen';
+
+  const ProgresScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
