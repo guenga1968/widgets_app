055ab2a797e31eba40b611cd1e3d102b8d46e532
@guenga1968 <geg1968@hotmail.com>
2023-06-20 13:37:54 -0300

    probando git

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
+// probando git
+
 void main() {
   runApp(const MainApp());
 }
