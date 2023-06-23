363344ece3bf93fd512d3e963176095d16e23dd9
@guenga1968 <geg1968@hotmail.com>
2023-06-23 16:26:16 -0300

    page view finalizado

--- lib/presentation/screens/app_tutorial/app_tutoria_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutoria_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SlideInfo {
   final String title;
@@ -11,36 +13,90 @@ class SlideInfo {
 final slides = <SlideInfo>[
   SlideInfo(
       'Buscar comida',
-      'Non amet in occaecat officia commodo minim sunt excepteur ullamco proident nostrud. Eiusmod eu eu velit cupidatat nisi et incididunt est eu. Duis veniam consectetur ullamco tempor nisi ipsum culpa mollit tempor do. Lorem irure aliquip sint cillum nostrud aute amet minim cupidatat magna duis cillum in sit. Ipsum mollit officia ea in mollit ea. Magna nostrud reprehenderit pariatur nostrud non dolor non magna aliqua dolor exercitation minim consectetur anim.',
+      'Magna nostrud reprehenderit pariatur nostrud non dolor non magna aliqua dolor exercitation minim consectetur anim.',
       'assets/images/1.png'),
   SlideInfo(
       'Entrega rapida',
-      'Nostrud cupidatat anim voluptate commodo est et veniam enim eu mollit qui Lorem ullamco. Non occaecat est duis consectetur anim anim do dolore do mollit. Voluptate Lorem laborum eu id excepteur eu quis consequat. Duis amet adipisicing sunt officia reprehenderit eiusmod sint ipsum consectetur officia quis ipsum tempor ipsum.',
+      'Duis amet adipisicing sunt officia reprehenderit eiusmod sint ipsum consectetur officia quis ipsum tempor ipsum.',
       'assets/images/2.png'),
   SlideInfo(
       'Disfruta la comida',
-      'Pariatur laboris consequat consectetur sint mollit. Sint Lorem excepteur proident deserunt fugiat. Ipsum qui eu et anim elit sint cupidatat ea duis ad exercitation consequat. Minim qui laboris in esse ipsum proident excepteur exercitation fugiat quis aute officia. Sunt occaecat ad deserunt dolor aliqua ex occaecat tempor mollit enim veniam elit. In nulla cupidatat id ipsum officia cillum. Lorem sint aliquip laboris aliquip consequat quis ad aliquip adipisicing irure ad.',
+      'In nulla cupidatat id ipsum officia cillum. Lorem sint aliquip laboris aliquip consequat quis ad aliquip adipisicing irure ad.',
       'assets/images/3.png'),
 ];
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const String name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      final page = pageController.page ?? 0;
+      if (!endReached && page >= (slides.length - 1.6)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: slides
-            .map(
-              (slideData) => _Slide(
-                  title: slideData.title,
-                  caption: slideData.caption,
-                  imageUrl: slideData.imageUrl),
-            )
-            .toList(),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                      title: slideData.title,
+                      caption: slideData.caption,
+                      imageUrl: slideData.imageUrl),
+                )
+                .toList(),
+          ),
+          Positioned(
+            top: 20,
+            right: 40,
+            child: TextButton(
+              onPressed: () => context.pop(),
+              child: const Text('Salir'),
+            ),
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 40,
+                  right: 40,
+                  child: FadeInRight(
+                    duration: const Duration(milliseconds: 500),
+                    child: FilledButton(
+                      onPressed: () => context.pop(),
+                      child: const Text('Comenzar'),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+        ],
       ),
     );
   }
@@ -55,13 +111,33 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
     return Padding(
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
       child: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image(
-              image: AssetImage(imageUrl),
+            Expanded(
+              child: Image.asset(
+                imageUrl,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              title,
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              caption,
+              style: captionStyle,
             ),
           ],
         ),
